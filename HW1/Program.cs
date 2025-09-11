b916f1df5a0035986957a7e39de1174974692b66
luongtrungchien-249 <luongtrungchienox@gmail.com>
2025-09-11 22:32:27 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,10 +1,29 @@
 ﻿internal class Program
 {
+     public string Name { get; set; }
+     public int MaSV { get; set; }
+     public string Class { get; set; }
+     public string UsernameGIT { get; set; }
+     public string EmailAddress { get; set; }
+
+     public Information() 
+     {
+         Name = " Lương Trung Chiến ";
+         MaSV = 12424042;
+         Class = "12424TN";
+         UsernameGIT = "luongtrungchien-249";
+         EmailAddress = "luongtrungchienox@gmail.com";
+     }
+     public void hienThi()
+     {
+         Console.WriteLine($" Họ tên : { Name } \t \t Mã sinh viên : { MaSV } \t \t Lớp : { Class } \t \t Username : { UsernameGIT } \t \t Email : { EmailAddress }");
+     }
     
     private static void Main(string[] args)
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Information infor = new Information();
+        infor.hienThi();
     }
 }
