00a6f902027285f9692c2b3994a02b249ff8def9
luongtrungchien-249 <luongtrungchienox@gmail.com>
2025-09-12 02:52:44 +0700

    Update Person.cs

--- HW2/Person.cs
+++ HW2/Person.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW2
+namespace studiesc_
 {
     internal class Person
     {
@@ -12,7 +12,7 @@ namespace HW2
         private string address;
         private double salary;
 
-        // Constructor
+        
         public Person(string name, string address, double salary)
         {
             this.name = name;
@@ -36,63 +36,51 @@ namespace HW2
             set { salary = value; }
         }
 
-        // Phương thức nhập thông tin người
+        
         public static Person Nhap(string name, string address, string sSalary)
         {
             double salary = 0;
-            bool validSalary = false;
 
-            // Kiểm tra tính hợp lệ của lương
-            while (!validSalary)
+            
+            if (string.IsNullOrWhiteSpace(sSalary))
             {
-                // Sử dụng double.TryParse để kiểm tra lương có phải là số hợp lệ không
-                if (double.TryParse(sSalary, out salary))
-                {
-                    // Kiểm tra lương có lớn hơn 0 không
-                    if (salary <= 0)
-                    {
-                        Console.WriteLine("Lương phải lớn hơn 0.");
-                    }
-                    else
-                    {
-                        validSalary = true;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Bạn phải nhập số.");
-                }
+                throw new Exception("Bạn phải nhập lương.");
+            }
 
-                // Nếu lương không hợp lệ, yêu cầu nhập lại
-                if (!validSalary)
-                {
-                    Console.Write("Vui lòng nhập lại lương: ");
-                    sSalary = Console.ReadLine();
-                }
+            if (!double.TryParse(sSalary, out salary))
+            {
+                throw new Exception("Bạn phải nhập số.");
+            }
+
+            if (salary <= 0)
+            {
+                throw new Exception("Lương phải lớn hơn 0.");
             }
 
             return new Person(name, address, salary);
         }
 
-        // Phương thức hiển thị thông tin người
+        
         public static void HienThi(Person person)
         {
-            Console.WriteLine($"\nThông tin người bạn đã nhập");
             Console.WriteLine($"Tên: {person.Name}");
             Console.WriteLine($"Địa chỉ: {person.Address}");
-            Console.WriteLine($"Lương: {person.Salary}");
+            Console.WriteLine($"Lương: {person.Salary:N0} VND");
+            Console.WriteLine("---------------------------");
         }
 
-        // Phương thức sắp xếp theo lương (BubbleSort)
-        public static Person[] sortBySalary(Person[] persons)
+        public static Person[] SortBySalary(Person[] persons)
         {
+            if (persons == null || persons.Length == 0)
+                return persons;
+
             for (int i = 0; i < persons.Length - 1; i++)
             {
                 for (int j = 0; j < persons.Length - i - 1; j++)
                 {
                     if (persons[j].Salary > persons[j + 1].Salary)
                     {
-                        // Hoán đổi vị trí nếu lương của person[j] lớn hơn person[j+1]
+                        
                         Person temp = persons[j];
                         persons[j] = persons[j + 1];
                         persons[j + 1] = temp;
